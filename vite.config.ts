da15aa17b68f0fa95e629336610b9d73363bb41b
Mikhail Konin <2heitz@mail.ru>
2022-09-05 17:39:36 +0300

    build: init default workspace

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
+// https://vitejs.dev/config/
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgrPlugin()],
+  /* If proxy is needed
+  server: {
+    proxy: {
+      "/api": "localhost:8080"
+    }
+  },
+  */
   build: {
     sourcemap: true,
   },
