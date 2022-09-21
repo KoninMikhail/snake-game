215261185c99faa45efa5f676a02bb7cc3654060
Heitz <2heiyz@mail.ru>
2022-09-21 20:32:47 +0300

    build: added jsdoc comments

--- src/components/UI/templates/Error/ErrorTemplate.tsx
+++ src/components/UI/templates/Error/ErrorTemplate.tsx
@@ -2,10 +2,24 @@ import { GridItem } from 'components/UI/extend/GridItem/GridItem';
 import { Wrapper } from 'components/UI/extend/Wrapper/Wrapper';
 import { StyledErrorTemplate, StyledErrorTemplateGrid } from './StyledErrorTemplate';
 
+/**
+ * @typedef IErrorTemplateProps
+ *
+ * @property {String} message
+ */
 interface IErrorTemplateProps {
   message: string;
 }
 
+/**
+ * Error Template
+ *
+ * @description Use for Error pages (AppError, 404, etc.)
+ *
+ * @param {IErrorTemplateProps} props - Error template properties
+ * @return {JSX.Element}
+ * @constructor
+ */
 export const ErrorTemplate = ({ message }: IErrorTemplateProps): JSX.Element => {
   return (
     <>
