59878a14de77709ca050acded9a45b927accc3f8
Mikhail Konin <dev.konin@gmail.com>
2022-10-12 21:50:24 +0300

    build: add redux store provider and hydrate from local storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { DataBrowserRouter } from 'react-router-dom';
-import routes from './routes';
+import { Provider } from 'react-redux';
+import routes from './routes/routes';
+import store, { persist } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
+// Prepare DOM
 const container = document.getElementById('root') as HTMLElement;
-
 const root = createRoot(container);
 
+// Start
 root.render(
   <StrictMode>
-    <DataBrowserRouter routes={routes} fallbackElement={<div>loading...</div>} />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persist}>
+        <DataBrowserRouter routes={routes} fallbackElement={<div>loading...</div>} />
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
