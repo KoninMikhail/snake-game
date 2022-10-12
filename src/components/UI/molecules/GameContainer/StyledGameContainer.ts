afcf981f778ef5db29cc74a51ab36acf00a9913a
Mikhail Konin <dev.konin@gmail.com>
2022-10-12 21:46:57 +0300

    build: removed old instances

--- src/components/UI/molecules/GameContainer/StyledGameContainer.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledGameContainer = styled.div<{ size: string }>`
-  display: block;
-  width: ${(props) => props.size};
-  height: ${(props) => props.size};
-`;
--- src/store.ts
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-const store = configureStore({
-  reducer: {},
-});
-
-export default store;
