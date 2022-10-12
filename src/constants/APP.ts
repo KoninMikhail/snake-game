06c4ee0dd4693dab5bcf5cf0db0c433b0051b9b4
Mikhail Konin <dev.konin@gmail.com>
2022-10-12 21:56:59 +0300

    build: add page transition timing

--- src/constants/APP.ts
+++ src/constants/APP.ts
@@ -1,9 +1,13 @@
+/* Settings */
+export const PAGE_TRANSITION_DURATION_MS = 800;
+
+/* DATA*/
 export const COPYRIGHT_BUILD_YEAR = 2022;
 export const COPYRIGHT_AUTHOR = 'Mikhail Konin';
 export const COPYRIGHT_LICENSE = 'MIT';
 export const GITHUB_LINK = 'https://github.com/KoninMikhail/react-snake-game';
 export const GITHUB_LINK_TEXT = 'Star on GitHub';
-export const ACTION_TEXT = 'press ENTER to start...';
-export const ACTION_TEXT_MOBILE = 'DOUBLE TAP to start...';
+export const ACTION_TEXT_DESKTOP = 'press ENTER to start...';
+export const ACTION_TEXT = 'DOUBLE TAP to start...';
 export const NOT_FOUND_TEXT = "Don't look for easter eggs, asshole!";
 export const ERR_TEXT = 'Error in app';
