86f4a00f67d3794413201bf868141d74c6341932
Mikhail Konin <dev.konin@gmail.com>
2022-10-12 20:43:54 +0300

    build: remove deprecated hooks

--- src/hooks/__tests__/use-counter.test.ts
+++
@@ -1,8 +0,0 @@
-import { renderHook } from '@testing-library/react';
-import useCounter from '../use-counter';
-
-test('should use counter', () => {
-  const { result } = renderHook(() => useCounter());
-  expect(result.current.count).toBe(0);
-  expect(typeof result.current.increment).toBe('function');
-});
