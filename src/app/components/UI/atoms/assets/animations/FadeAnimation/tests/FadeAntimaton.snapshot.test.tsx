e8b6b5cb56f374910dac8fb67f64f0fb201a1ab1
Mikhail Konin <dev.konin@gmail.com>
2023-02-19 09:29:09 +0300

    build: add animations

---
+++ src/app/components/UI/atoms/assets/animations/FadeAnimation/tests/FadeAntimaton.snapshot.test.tsx
@@ -0,0 +1,29 @@
+import {render} from '@testing-library/react';
+import {FadeAnimation} from '../FadeAnimation';
+import {IBaseTheme} from '@styles/theme';
+
+/**
+ * Mocks
+ */
+const fakeTheme = {
+    animations: {
+        fade: {
+            duration: 800,
+            count: 1,
+            timing: 'fade',
+            fill: 'forwards',
+        },
+    },
+} as unknown as IBaseTheme;
+
+/**
+ * Tests
+ */
+describe('FadeAnimation', () => {
+    it('equal snapshot', () => {
+        const { container } = render(
+            <FadeAnimation direction={'in'} delay={500} theme={fakeTheme} />
+        );
+        expect(container).toMatchSnapshot();
+    });
+});
