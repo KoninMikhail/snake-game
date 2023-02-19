897d59e29fa05f442e11a3e2b13b45bfc7da742a
Mikhail Konin <dev.konin@gmail.com>
2023-02-19 09:40:23 +0300

    refactor: typography - text

--- src/components/UI/atoms/body/TextAlign/StyledTextAlign.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { ITextAlignProps } from './TextAlign';
-
-export const StyledTextAlign = styled.div<ITextAlignProps>`
-  text-align: ${(props) => props.align};
-`;
