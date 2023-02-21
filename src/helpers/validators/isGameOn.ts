abef0d68868b8c98dd867d2b24c01144f2cfa531
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 04:14:42 +0300

    build: check that the game is currently running

---
+++ src/helpers/validators/isGameOn.ts
@@ -0,0 +1,14 @@
+import { GameStatus } from '@store/slices/game.slice';
+
+/**
+ * @function isGameOn
+ * 
+ * If the gameStatus is GAME, return true, otherwise return false.
+ * 
+ * @param gameStatus - GameStatus
+ * @returns A function that takes a gameStatus and returns a boolean.
+ */
+const isGameOn = (gameStatus: GameStatus) => {
+    return gameStatus === 'GAME';
+};
+export default isGameOn;
