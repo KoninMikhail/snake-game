d64eca59f894239e393749db0dc55f32bf2b5659
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 03:52:55 +0300

    build: create type color

---
+++ src/types/color.ts
@@ -0,0 +1,10 @@
+import {CssNamedColors} from '@/types/enums/css-named-colors.enum';
+
+/**
+ * @name Color
+ * @author Mikhail Konin <dev.konin@gmail.com>
+ *
+ * Use it for color check
+ */
+
+export type Color = `${CssNamedColors}` | `#${string}`;
