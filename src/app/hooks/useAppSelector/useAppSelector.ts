59f3f24fd6c9e5bad2db1039bb7cc837fda747c8
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 04:35:07 +0300

    build: create useAppSelector hook

---
+++ src/app/hooks/useAppSelector/useAppSelector.ts
@@ -0,0 +1,6 @@
+import type { TypedUseSelectorHook } from 'react-redux';
+import { useSelector } from 'react-redux';
+import type { RootState } from '@store/store';
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export default useAppSelector;
