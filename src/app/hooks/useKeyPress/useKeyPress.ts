0d971c2bf6ff61ed3307ab5fe9c12da718756359
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 04:37:01 +0300

    build: create useKeyPress hook

---
+++ src/app/hooks/useKeyPress/useKeyPress.ts
@@ -0,0 +1,25 @@
+import { useEventListener } from 'usehooks-ts';
+
+/**
+ * @name useKeyPress
+ * @author Mikhail Konin <dev.konin@gmail.com>
+ * 
+ * Use it for bind callbacks on key press
+ * 
+ * It takes an array of keys and a callback function, and calls the callback function whenever any of
+ * the keys are pressed
+ * 
+ * @param keys - An array of keys that we want to listen for.
+ * @param callback - (event: KeyboardEvent) => void
+ */
+const useKeyPress = <K>(keys: K[], callback: (event: KeyboardEvent) => void): void => {
+    const handleEvent = (event: KeyboardEvent) => {
+        if (keys.some((key) => event.key === key)) {
+            callback(event);
+        }
+    }
+    
+    useEventListener('keydown', handleEvent);
+};
+
+export default useKeyPress;
