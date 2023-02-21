f6d03dc42aaca8ee65d66678a5f1b630f23fd76a
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 05:01:30 +0300

    build: add score

---
+++ src/app/components/UI/molecules/information/Score/tests/Score.unit.test.tsx
@@ -0,0 +1,20 @@
+import {screen} from '@testing-library/react';
+import {Score} from '../Score';
+import padCharactersLeft from '@utils/padCharactersLeft/padCharactersLeft';
+import {renderWithProviders} from '@utils/test-utils';
+import {GameData} from '@store/slices/game.slice';
+
+/**
+ * Tests
+ */
+describe('Score', () => {
+    const mockScore = Math.floor(Math.random() * 30);
+
+    it('correctly renders', () => {
+        const initialState = {
+            preloadedState: { game: { score: mockScore } as GameData },
+        };
+        renderWithProviders(<Score align={'left'} />, initialState);
+        expect(screen.getByText(padCharactersLeft(mockScore, 6))).toBeInTheDocument();
+    });
+});
