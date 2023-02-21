5d6c08274ed9a441ec6483cb1e9b8586e3d4897f
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 04:19:00 +0300

    build: create constants

---
+++ src/constants/APP.ts
@@ -0,0 +1,8 @@
+// Global
+export const SOUND_ENABLED = true;
+export const PAGE_TRANSITION_DURATION = 1400;
+
+// PATHS
+export const ASSETS_DIR = 'assets';
+export const IMAGES_DIR = 'images';
+export const SOUNDS_DIR = 'sounds';
