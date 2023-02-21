62fef6e69e503e7613bbacbff9ff109062a98a7e
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 04:30:24 +0300

    build: create utils for get values from theme

---
+++ src/helpers/theme/getThemeIconSize.ts
@@ -0,0 +1,17 @@
+import { IBaseTheme } from '@styles/theme';
+import { ThemeIcons } from '@styles/theme_parts/icons';
+
+/**
+ * @name getThemeIconSize
+ * 
+ * Get the icon size from the theme.
+ * 
+ * @param theme - The theme object
+ * @param size - The size of the icon you want to get.
+ * @returns A function that takes a theme and a size and returns the theme.icons[size]
+ */
+const getThemeIconSize: GetFromThemeFn<IBaseTheme, ThemeIcons> = (theme, size) => {
+    return theme.icons[size];
+};
+
+export default getThemeIconSize;
