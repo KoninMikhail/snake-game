bc3bb557e1268c3bd2da3a7c2adec135cb8d213a
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 04:14:02 +0300

    build: check that the device has a touch screen

---
+++ src/helpers/validators/isTouchDevice.ts
@@ -0,0 +1,8 @@
+/**
+ * @function isTouchDevice
+ * 
+ * If the browser supports touch events, return true, otherwise return false.
+ * 
+ * @returns A boolean value.
+ */
+export const isTouchDevice = (): boolean => 'ontouchstart' in window || navigator.maxTouchPoints > 0;
