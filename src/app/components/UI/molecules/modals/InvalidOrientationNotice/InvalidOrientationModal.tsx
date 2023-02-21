9c81df69690dc4140c0fa39e716cd03c89793da5
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 05:02:40 +0300

    build: add invalid orientation modal

---
+++ src/app/components/UI/molecules/modals/InvalidOrientationNotice/InvalidOrientationModal.tsx
@@ -0,0 +1,39 @@
+import {
+    StyledInvalidOrientationModalContainer,
+    StyledInvalidOrientationModalContent,
+    StyledInvalidOrientationNoticel,
+} from './style/StyledInvalidOrientationNoticel';
+import {GlassesIcon} from '@ui/atoms/assets/icons/GlassesIcon/GlassesIcon';
+import {PrimaryFill} from '@ui/atoms/assets/fills/PrimaryFill/PrimaryFill';
+import {FadeAnimation} from '@ui/atoms/assets/animations/FadeAnimation/FadeAnimation';
+import {PrimaryTextShadow} from '@ui/atoms/assets/shadows/text/PrimaryTextShadow/PrimaryTextShadow';
+import {Caption} from '@ui/atoms/typography/caption/Caption/Caption';
+import {
+    IInvalidOrientationModal
+} from '@ui/molecules/modals/InvalidOrientationNotice/InvalidOrientationModal.interface';
+
+/**
+ * @name InvalidOrientationModal
+ * @author Mikhail Konin <dev.konin@gmail.com>
+ *
+ * Use to ready a message about incorrect screen orientation.
+ *
+ * @param children - message for user
+ * @returns {JSX.Element}
+ */
+export const InvalidOrientationModal = ({ message }: IInvalidOrientationModal) => (
+    <StyledInvalidOrientationNoticel>
+        <FadeAnimation direction={'in'}>
+            <StyledInvalidOrientationModalContainer>
+                <StyledInvalidOrientationModalContent>
+                    <PrimaryTextShadow>
+                        <Caption align={'center'}>{message}</Caption>
+                    </PrimaryTextShadow>
+                    <PrimaryFill>
+                        <GlassesIcon size={'lg'} style={{ width: 'auto' }} />
+                    </PrimaryFill>
+                </StyledInvalidOrientationModalContent>
+            </StyledInvalidOrientationModalContainer>
+        </FadeAnimation>
+    </StyledInvalidOrientationNoticel>
+);
