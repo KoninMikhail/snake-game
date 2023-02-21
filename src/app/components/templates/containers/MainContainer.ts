14c96af62212d6de647e95291b121d44df3b0529
Mikhail Konin <dev.konin@gmail.com>
2023-02-21 05:09:17 +0300

    build: add containers

---
+++ src/app/components/templates/containers/MainContainer.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import {up} from 'styled-breakpoints';
+
+/**
+ * @name MainContainer
+ * @version 1.0
+ * @author Mikhail Konin <dev.konin@gmail.com>
+ *
+ * Use for create main content container
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+export const MainContainer = styled.div`
+    display: grid;
+    position: relative;
+    grid-template-rows: max-content auto max-content;
+    gap: ${({ theme }) => theme.gaps.md};
+    width: 100%;
+    height: 100%;
+    padding: 1.5em;
+
+    ${up('md')} {
+        padding: 2em;
+    }
+
+    ${up('lg')} {
+        padding: 5vh 4vw;
+    }
+`;
