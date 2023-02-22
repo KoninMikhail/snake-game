be862e224ee10c638c0f28c02def77b506a3bf4e
Mikhail Konin <dev.konin@gmail.com>
2023-02-23 02:35:59 +0300

    build: add DeviceTouch Type

--- src/types/device.ts
+++ src/types/device.ts
@@ -4,8 +4,11 @@ export type DeviceOrientation = 'portrait' | 'landscape';
 
 export type DeviceModel = string;
 
+export type DeviceTouch = boolean;
+
 export type DeviceInfo = {
     orientation: DeviceOrientation | null;
     screen: Device | null;
     model: DeviceModel | null;
+    touch: DeviceTouch;
 };
