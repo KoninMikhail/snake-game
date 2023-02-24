ae21e348442c70f987b70dac61cea65a3c154076
Mikhail Konin <dev.konin@gmail.com>
2023-02-24 06:24:09 +0300

    refactor: detect a touch device from app state

--- src/app/components/pages/Game/GamePage.tsx
+++ src/app/components/pages/Game/GamePage.tsx
@@ -1,9 +1,9 @@
 import useAppSelector from '@hooks/useAppSelector/useAppSelector';
-import {useTranslation} from 'react-i18next';
-import {maxScoreSelector} from '@store/slices/maxScore.slice';
-import {GamePageTemplate} from '@templates/pages/Game/GamePageTemplate';
-import {GamePageSounds} from '@sounds/pages/GamePageSounds';
-import {isTouchDevice} from '@/helpers/validators/isTouchDevice';
+import { useTranslation } from 'react-i18next';
+import { maxScoreSelector } from '@store/slices/maxScore.slice';
+import { GamePageTemplate } from '@templates/pages/Game/GamePageTemplate';
+import { GamePageSounds } from '@sounds/pages/GamePageSounds';
+import { appDeviceTouchStateSelector } from '@store/slices/app.slice';
 
 /**
  * @name GamePage
@@ -14,6 +14,7 @@ import {isTouchDevice} from '@/helpers/validators/isTouchDevice';
  */
 export const GamePage = () => {
     const { t } = useTranslation();
+    const isTouchDevice = useAppSelector(appDeviceTouchStateSelector);
 
     /** MaxScore
      * ======================== */
@@ -31,9 +32,14 @@ export const GamePage = () => {
     const modalGameOverHeadline = t('game.modal.gameOver.caption');
     const modalGameOverButtonLabel = t('game.modal.gameOver.button');
     const modalGamePauseHeadline = t('game.modal.gamePause.caption');
-    const modalGamePauseMessage = isTouchDevice()
+    const modalGamePauseMessage = isTouchDevice
         ? t('game.modal.gamePause.message')
         : t('game.modal.gamePause.message.desktop');
+
+    /** Controls Message
+     * ======================== */
+    const infoMessageSwipeControl = t('game.messages.control.swipe');
+    const infoMessageKeyboardControl = t('game.messages.control.keyboard');
 
     /** Invalid orientation modal message
      * ======================== */
@@ -74,8 +80,8 @@ export const GamePage = () => {
                             },
                         },
                         messages: {
-                            swipeControl: t('game.messages.control.swipe'),
-                            keyboardControl: t('game.messages.control.keyboard'),
+                            swipeControl: infoMessageSwipeControl,
+                            keyboardControl: infoMessageKeyboardControl,
                         },
                         maxScore: {
                             value: userMaxScore,
