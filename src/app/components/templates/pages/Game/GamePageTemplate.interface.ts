f334f93cc86894fea1793a3b5a8021b9e8b7bf9b
Mikhail Konin <dev.konin@gmail.com>
2023-02-24 06:15:22 +0300

    build: update template

--- src/app/components/templates/pages/Game/GamePageTemplate.interface.ts
+++ src/app/components/templates/pages/Game/GamePageTemplate.interface.ts
@@ -4,10 +4,7 @@ import { AssertPositive } from '@/types/assert-positive';
 import { IGameTemplate } from '@templates/sections/Game/GameTemplate.interface';
 import { IToolbarWithSoundControlAndLevel } from '@templates/toolbars/ToolbarWithSoundControlAndLevel/ToolbarWithSoundControlAndLevel.interface';
 
-export interface IGamePageTemplate<
-    T extends AssertPositive<number>,
-    K extends AssertPositive<number>
-> {
+export interface IGamePageTemplate<T extends AssertPositive<number>> {
     toolbar: IToolbarWithSoundControlAndLevel;
     game: IGameTemplate<T>;
     footer: IFooter;
