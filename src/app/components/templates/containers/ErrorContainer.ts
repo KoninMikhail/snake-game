d00490d91a8afcbc01012400e43b9c1a133f3cb0
Mikhail Konin <dev.konin@gmail.com>
2023-02-24 08:00:45 +0300

    build: create error container

---
+++ src/app/components/templates/containers/ErrorContainer.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { up } from 'styled-breakpoints';
+
+/**
+ * @name MainContainer
+ * @version 1.0
+ * @author Mikhail Konin <dev.konin@gmail.com>
+ *
+ * Use for create main content container
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+export const ErrorContainer = styled.div`
+    display: grid;
+    position: relative;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    align-items: center;
+    justify-content: center;
+    gap: ${({ theme }) => theme.gaps.md};
+    width: 100%;
+    height: 100%;
+    padding: 1.5em;
+
+    ${up('md')} {
+        padding: 2em;
+    }
+
+    ${up('lg')} {
+        padding: 5vh 4vw;
+    }
+`;
